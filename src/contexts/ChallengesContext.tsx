4ab78a3e8a7fdf48fa174722b87e356ba2534973
mhrocha1997 <marcelo.h.rocha1997@gmail.com>
2021-02-25 08:42:52 -0300

    [ADD] Notificações

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from 'react';
+import { createContext, useState, ReactNode, useEffect } from 'react';
 import challenges from '../../challenges.json';
 
 interface Challenge{
@@ -33,7 +33,9 @@ export function ChallengesProvider({children}: ChallengesProviderProps){
     const [activeChallenge, setActiveChallenge] = useState(null);
 
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
-
+    useEffect(() => {
+        Notification.requestPermission();
+    }, [])
     function levelUp(){
         setLevel(level + 1);
     }
@@ -44,6 +46,14 @@ export function ChallengesProvider({children}: ChallengesProviderProps){
         const challenge = challenges[randomChallengeIndex];
 
         setActiveChallenge(challenge);
+
+        new Audio('/notification.mp3').play();
+
+        if(Notification.permission === 'granted') {
+            new Notification('Novo desafio!',{
+                body: `Valendo ${challenge.amount}xp`
+            })
+        }
     }
 
     function resetChallenge(){
